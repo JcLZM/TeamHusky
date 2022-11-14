c1e4222214e56b31ee0b235eb403911316b7b069
JcLZM <boxofpaperclip@gmail.com>
2022-11-15 00:45:54 +0800

    Adding functionality to view review buttons. no popup yet.
    Adding functionality to view review buttons.
    
    no popup yet.

--- boundary/authordashboard.php
+++ boundary/authordashboard.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+function displayAuthorDashboardUI(){
+?>
 <title>Author's Dashboard - TeamHusky Conference Systems</title>
 <head>
     <div class="topnav">
@@ -10,6 +13,7 @@
             
         </span>
         <span style = "float:right; color:#f4f4f4;">Hello, AUTHORNAME, AUTHORID</span>
+        <!-- should load from db??-->
 
     </div>
     <style>
@@ -87,32 +91,79 @@
         </style>
     <script>
         function submitPaper(){ //creates the popup
-            var popup = document.getElementById("submitpaper")
+            var popup = document.getElementById("submitpaper");
             popup.style.display = "block";
         }
 
-        function cancelPaperSubmit(){
-            var popup = document.getElementById("submitpaper")
+        function cancelPaperSubmit(){ 
+            //closes popup
+            //should clear all fields as well
+            var popup = document.getElementById("submitpaper");
             popup.style.display = "none";
+            var papername = document.getElementById("papername");
+            var authors = document.getElementById("authors");
+            var papercontent = document.getElementById("papercontent");
+            papername.value = "";
+            authors.value = "";
+            papercontent.value = "";
         }
 
         function confirmPaperSubmit(){
-            var popup = document.getElementById("submitpaper")
+            var popup = document.getElementById("submitpaper");
             popup.style.display = "none";
             //store all the data in variables and submit it
             var papername = document.getElementById("papername");
+            var authors = document.getElementById("authors");
+            var papercontent = document.getElementById("papercontent");
+            
+            //SOME CODE HERE JIAHAO LOOK AT IT
+            
+            //also clear it
+            papername.value = "";
+            authors.value = "";
+            papercontent.value = "";
         }
 
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
-            cancelPaperSubmit();
-}
+            var popup = document.getElementById("submitpaper");
+            if (event.target == popup) {
+                popup.style.display = "none";
+                var papername = document.getElementById("papername");
+                var authors = document.getElementById("authors");
+                var papercontent = document.getElementById("papercontent");
+                papername.value = "";
+                authors.value = "";
+                papercontent.value = "";
+                    
+            }
+        }
+
+
+        //Click to view reviews function
+        
+        function viewReviews(buttonID){
+            //1.Get Paper Number(Should have been loaded in page.)
+            var number = buttonID.substr(buttonID.length - 1);
+            //alert(number)
+            var paperNum = "num" + number
+            //alert(paperNum)
+            var paperID = document.getElementById(paperNum).innerHTML
+            //alert(paperID)
+            var statusNum = status + number
+            var status = document.getElementById(statusNum).innerHTML
+            if (status == "Pending Review"){
+                return;
+            } // if there are no reviews, end function here.
+            //submit request to database and retrieve review (content, and the reviewer)
+
+            //load it in, as a modal
+            //allow rating
+        }
     </script>
 </head>
 
-<?php
-function displayAuthorDashboardUI(){
-?>
+
 
 <body>
 
@@ -124,15 +175,15 @@ function displayAuthorDashboardUI(){
             <table class="modal-content">
                 <tr>
                     <td>Name of paper:</td>
-                    <td><input type="text" id = "papername"></td>
+                    <td><input style = width:500px type="text" id = "papername"></td>
                 </tr>
                 <tr>
                     <td>Authors:</td>
-                    <td><input type="text"></td>
+                    <td><input style = width:500px type="text" id = "authors"></td>
                 </tr>
                 <tr>
-                    <td>Paper Content</td>
-                    <td><input type="text"></td>
+                    <td>Paper Content:</td>
+                    <td><textarea style="height:200px;width:500px;resize:none;" id = "papercontent"></textarea></td>
                 </tr>
                 <tr>
                     <td><button onclick="confirmPaperSubmit()">Confirm</button></td>
@@ -153,32 +204,75 @@ function displayAuthorDashboardUI(){
                 <td>Reviews</td>
              </tr>
             <tr> 
-                <td>0000017</td> <!--Paper number from database-->
-                <td>Email: a vector of contamination</td><!-- Paper title from database-->
-                <td>name of author</td><!-- Author(s)-->
-                <td>Pending Review</td><!-- status of paper-->
-                <td>None</td>
+                <td id = "num1">0000017</td> <!--Paper number from database-->
+                <td id = "name1">Email: a vector of contamination</td><!-- Paper title from database-->
+                <td id = "author1">name of author</td><!-- Author(s)-->
+                <td id = "status1">Pending Review</td><!-- status of paper-->
+                <td id = "button1" onClick="viewReviews(this.id)">None</td><!-- button should check status -->
             </tr>
             <tr> 
-                <td>0000018</td> <!--Paper number from database-->
-                <td>Email: a donut</td><!-- Paper title from database-->
-                <td>name of author</td><!-- Author(s)-->
-                <td>Reviewed</td><!-- status of paper-->
-                <td><button>click to view</button></td> <!-- should pull reviews from db-->
+                <td id = "num2">0000027</td> <!--Paper number from database-->
+                <td id = "name2">Email: a vector of contamination</td><!-- Paper title from database-->
+                <td id = "author2">name of author</td><!-- Author(s)-->
+                <td id = "status2">Pending Review</td><!-- status of paper-->
+                <td id = "button2" onClick="viewReviews(this.id)"><button>click to view</button></td><!-- button should check status -->
+                <!-- should pull reviews from db-->
             </tr>
             <tr> 
-                <td>0000019</td> <!--Paper number from database-->
-                <td>Tomato paste</td><!-- Paper title from database-->
-                <td>name of author</td><!-- Author(s)-->
-                <td>Pending Review</td><!-- status of paper-->
-                <td><button>click to view</button></td> <!-- should pull reviews from db-->
+                <td id = "num3">0000037</td> <!--Paper number from database-->
+                <td id = "name3">Email: a vector of contamination</td><!-- Paper title from database-->
+                <td id = "author3">name of author</td><!-- Author(s)-->
+                <td id = "status3">Pending Review</td><!-- status of paper-->
+                <td id = "button3" onClick="viewReviews(this.id)"><button>click to view</button></td><!-- button should check status -->
+                <!-- should pull reviews from db-->
             </tr>
             <tr> 
-                <td>0000020</td> <!--Paper number from database-->
-                <td>Red wiles</td><!-- Paper title from database-->
-                <td>name of author</td><!-- Author(s)-->
-                <td>Pending Review</td><!-- status of paper-->
-                <td><button>click to view</button></td> <!-- should pull reviews from db-->
+                <td id = "num4">0000047</td> <!--Paper number from database-->
+                <td id = "name4">Email: a vector of contamination</td><!-- Paper title from database-->
+                <td id = "author4">name of author</td><!-- Author(s)-->
+                <td id = "status4">Pending Review</td><!-- status of paper-->
+                <td id = "button4" onClick="viewReviews(this.id)"><button>click to view</button></td><!-- button should check status -->
+                <!-- should pull reviews from db-->
+            </tr>
+            <tr> 
+                <td id = "num5">0000057</td> <!--Paper number from database-->
+                <td id = "name5">Email: a vector of contamination</td><!-- Paper title from database-->
+                <td id = "author5">name of author</td><!-- Author(s)-->
+                <td id = "status5">Pending Review</td><!-- status of paper-->
+                <td id = "button5" onClick="viewReviews(this.id)"><button>click to view</button></td><!-- button should check status -->
+                <!-- should pull reviews from db-->
+            </tr>
+            <tr> 
+                <td id = "num6">0000067</td> <!--Paper number from database-->
+                <td id = "name6">Email: a vector of contamination</td><!-- Paper title from database-->
+                <td id = "author6">name of author</td><!-- Author(s)-->
+                <td id = "status6">Pending Review</td><!-- status of paper-->
+                <td id = "button6" onClick="viewReviews(this.id)"><button>click to view</button></td><!-- button should check status -->
+                <!-- should pull reviews from db-->
+            </tr>
+            <tr> 
+                <td id = "num7">0000067</td> <!--Paper number from database-->
+                <td id = "name7">Email: a vector of contamination</td><!-- Paper title from database-->
+                <td id = "author7">name of author</td><!-- Author(s)-->
+                <td id = "status7">Pending Review</td><!-- status of paper-->
+                <td id = "button7" onClick="viewReviews(this.id)"><button>click to view</button></td><!-- button should check status -->
+                <!-- should pull reviews from db-->
+            </tr>
+            <tr> 
+                <td id = "num8">0000077</td> <!--Paper number from database-->
+                <td id = "name8">Email: a vector of contamination</td><!-- Paper title from database-->
+                <td id = "author8">name of author</td><!-- Author(s)-->
+                <td id = "status8">Pending Review</td><!-- status of paper-->
+                <td id = "button8" onClick="viewReviews(this.id)"><button>click to view</button></td><!-- button should check status -->
+                <!-- should pull reviews from db-->
+            </tr>
+            <tr> 
+                <td id = "num9">0000087</td> <!--Paper number from database-->
+                <td id = "name9">Email: a vector of contamination</td><!-- Paper title from database-->
+                <td id = "author9">name of author</td><!-- Author(s)-->
+                <td id = "status9">Pending Review</td><!-- status of paper-->
+                <td id = "button9" onClick="viewReviews(this.id)"><button>click to view</button></td><!-- button should check status -->
+                <!-- should pull reviews from db-->
             </tr>
         </table>
     </div> 
