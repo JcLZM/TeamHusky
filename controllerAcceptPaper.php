9c1184830a94eddf0ffa51c98537481eb12adcca
ewmgoh001 <104259667+ewmgoh001@users.noreply.github.com>
2022-11-17 22:08:18 +0800

    Add files via upload

---
+++ controllerAcceptPaper.php
@@ -0,0 +1,33 @@
+<?php
+include 'ChairmanHomeUI.php';
+include "classChairman.php";
+
+//Session start
+session_start();
+
+class controllerAcceptPaper{
+    // User Story 17 - Accept a paper
+    public function acceptPaper($paper_id){
+        $chairman = new Chairman;
+
+        if (isset($_POST['update'])){
+            $paper_id = $_POST['paper_id'];
+            $_SESSION["paper_id"] = $paper_id;
+
+            $result = $chairman->chairman_accept_paper($paper_id);
+            return $result;
+        }
+    }
+}
+
+$function = new controllerAcceptPaper;
+$acceptResult = $function->acceptPaper($_POST['paper_id']);
+if($acceptResult)
+{
+    echo
+    ("<script LANGUAGE='JavaScript'> 		
+        window.alert('User Updated Successfully!');
+        window.location.href='ChairmanHomeUI.php';
+    </script>");
+}
+?>
