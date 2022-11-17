680f7bb6d2cde7092211772b77cff4316d6c4887
JcLZM <boxofpaperclip@gmail.com>
2022-11-17 20:58:05 +0800

    s

--- classAuthor.php
+++ classAuthor.php
@@ -1,14 +1,21 @@
 <?php
 include 'classUser.php';
-class author extends user
-{
+class Author extends User
+{   
     public function author_submit_paper($author_id, $title, $content)
     {
         $conn = OpenCon();
         $query = "INSERT INTO papers (author_id, title, content, paper_status) 
-        VALUES('$author_id', '$title', '$content', 'Review pending')";
+        VALUES('$author_id', '$title', '$content', 'Review Pending')";
         $result = mysqli_query($conn, $query) or die(mysqli_connect_errno() . "Submission unsuccessful");
         return $result; // If successful, returns 1 row
+    }
+
+    public function author_view_review($paperId){
+        $conn = OpenCon();
+        $query = "SELECT * FROM reviews where review_id in (SELECT review_id FROM papers where paper_id = $paperId)LIMIT 9";
+        $result = mysqli_query($conn, $query) or die(mysqli_connect_errno() . "Submission unsuccessful");
+        return $result; // If successful, all reviews
     }
 
     public function author_delete_paper($author_id, $paper_id)
@@ -21,7 +28,7 @@ class author extends user
 
     public function author_view_rating($author_id, $paper_id)
     {
-        $conn = opencon();
+        $conn = OpenCon();
         $query1 = "SELECT review_id FROM papers WHERE paper_id = '$paper_id' AND author_id = '$author_id'";
         $result1 = mysqli_query($conn, $query1) or die(mysqli_connect_errno() . "Paper not found");
         $query2 = "SELECT reviewer_rating FROM reviews WHERE review_id = '$result1'";
@@ -31,15 +38,15 @@ class author extends user
 
     public function author_search_paper($author_id)
     {
-        $conn = opencon();
-        $query = "SELECT * FROM papers WHERE author_id = '$author_id'";
+        $conn = OpenCon();
+        $query = "SELECT * FROM papers WHERE author_id = '$author_id' ORDER BY paper_id DESC LIMIT 9";
         $result = mysqli_query($conn, $query) or die(mysqli_connect_errno() . "No papers found");
         return $result;
     }
 
     public function author_rate_review($author_id, $paper_id, $author_rating)
     {
-        $conn = opencon();
+        $conn = OpenCon();
         $query1 = "SELECT review_id FROM papers WHERE paper_id = '$paper_id' AND author_id = '$author_id'";
         $result1 = mysqli_query($conn, $query1) or die(mysqli_connect_errno() . "Paper not found");
         $query2 = "UPDATE reviews SET author_rating = '$author_rating' WHERE review_id = '$result1'";
