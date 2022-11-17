48f38b59017e5a3b32fd820715890c151382d311
Lim Yan Zhen Grace <99limgrace@gmail.com>
2022-11-17 20:55:26 +0800

    Update classChairman.php

--- classChairman.php
+++ classChairman.php
@@ -4,11 +4,10 @@ include 'classUser.php';
 class classChairman extends User{
 
     // User Story #23 - So that I can make changes
-    public function deleteBids($bidToBeDeleted){
-        $errorMsg = '';
+    public function chairman_delete_bid($userid, $bid_id){
         $conn = OpenCon();
-        $sql = "DELETE FROM bids WHERE bid_id = '$bidToBeDeleted'";
-        $result = mysqli_query($conn,$sql) or die(mysqli_connect_errno()."Data cannot deleted");
+        $sql = "DELETE FROM bids WHERE reviewer_id = '$userid' AND bid_id = '$bid_id'";
+        $result = mysqli_query($conn,$sql) or die(mysqli_connect_errno()."Bid cannot be deleted");
         if ($result !== FALSE){
             return $result; // If delete is successful, it will return 1
         }
@@ -18,9 +17,9 @@ class classChairman extends User{
     }
 
     // User Story #24 - So that when assigning the paper I can take into account of reviewer bid
-    public function viewBids(){
+    public function chairman_view_bid($userid){
         $conn = OpenCon();
-        $query = 'SELECT * FROM bids';
+        $query = "'SELECT * FROM bids WHERE reviewer_id = '$userid'";
         $result = mysqli_query($conn, $query) or die(mysqli_connect_errno()."SQL Error");
         return $result;
     }
